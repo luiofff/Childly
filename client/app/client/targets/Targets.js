9a721e5a388c7ad2f634270780a928fef3a5f3a8
luiofff <95875204+luiofff@users.noreply.github.com>
2023-12-11 23:20:56 +0300

    moving between elements

--- client/app/client/targets/Targets.js
+++ client/app/client/targets/Targets.js
@@ -1,13 +1,13 @@
 import styles from "./Tasks.module.css"
 import Card from "../components/Card"
 
-export default function Targets() {
+export default function Targets({ option }) {
   return (
     <>
         <section className={styles.content}>
           <span className={styles.title}>Цели</span>
           <div className={styles.card_space}>
-            <Card option={"targets"} money={21000} target_price={21000}/>
+            <Card option={option} money={21000} target_price={21000}/>
           </div>
         </section>
     </>
--- client/app/client/tasks/Tasks.js
+++ client/app/client/tasks/Tasks.js
@@ -1,13 +1,13 @@
 import styles from "./Tasks.module.css"
 import Card from "../components/Card"
 
-export default function Tasks() {
+export default function Tasks({ option }) {
     return (
       <>
         <section className={styles.content}>
           <span className={styles.title}>Активные</span>
           <div className={styles.card_space}>
-            <Card option={"tasks"} money={21000} target_price={1210000}/>
+            <Card option={option} money={21000} target_price={1210000}/>
           </div>
         </section>
       </>
